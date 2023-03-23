86b4e823174dd16abf3be52d89b9cda28ea554fd
shendoalex <shendo.alex@gmail.com>
2023-03-23 20:24:12 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error0 = new Error();
+        Error error1 = new Error(true, 1, "Message 1");
+        Error error2 = new Error(false, 2, "Message 2");
+        Error error3 = new Error(true, 3, "Message 3");
+        error0.printInfo();
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
