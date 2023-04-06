b0e3b996957c190af272b855ac9fdc89c367e2fd
shendoalex <shendo.alex@gmail.com>
2023-04-06 08:58:49 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -32,6 +32,9 @@ public class PasswordValidator {
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
             }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
+            }
         }
 
         boolean passwordContainsForbiddenWord = false;
