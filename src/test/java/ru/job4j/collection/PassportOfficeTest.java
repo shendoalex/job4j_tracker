c9662ad657a1825db7042970d049f6aa73347746
shendoalex <shendo.alex@gmail.com>
2023-04-17 20:57:19 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PassportOfficeTest {
+    @Test
+    public void whenTestAddMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
+    }
+
+    @Test
+    public void whenTestAddDoubleMethod() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Vasily Pupkine");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        assertThat(office.add(citizen2)).isFalse();
+    }
+}
