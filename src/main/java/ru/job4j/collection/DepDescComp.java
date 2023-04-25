af08925ccfc43e0a36c1afa835d38719b6a9860c
shendoalex <shendo.alex@gmail.com>
2023-04-25 12:52:04 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        int rsl = 0;
+        String[] a = o1.split("/", 2);
+        String[] b = o2.split("/", 2);
+        int compare = b[0].compareTo(a[0]);
+        return compare == 0 ? o1.compareTo(o2) : compare;
+    }
+}
---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String startCode = "";
+            for (String currentCode : value.split("/")) {
+                tmp.add(startCode + currentCode);
+                startCode += currentCode + "/";
+            }
+        }
+
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
