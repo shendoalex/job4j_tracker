7ce9b7f19645e6dfa2b5a4107cae59b47376f493
shendoalex <shendo.alex@gmail.com>
2023-05-15 20:55:40 +0300

    FilterNegativeNumbers

--- src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 public class FilterNegativeNumbers {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, -4, 3, 6, -6, 10, 0);
-        List<Integer> positive = numbers.stream().filter(num -> num > 0).collect(Collectors.toList());
+        List<Integer> positive = numbers.stream().filter(num -> num > 0).toList();
         positive.forEach(System.out::println);
     }
 }
